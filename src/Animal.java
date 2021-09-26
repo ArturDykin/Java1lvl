4b887eb93885ff9d2e35ceacbb9674ae60319dd7
Dykin <Dykin.artur@mail.ru>
2021-09-26 18:46:22 +0300

    Home Work 6

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+/**
+ * Java 1. Home work 6
+ *
+ * @author Dykin Artur
+ * @version 26.09.2021
+ */
+
+abstract class Animal implements IAnimals{
+    protected int distanceLength, swimLength;
+
+    Animal (int distanceLength, int swimLength){
+        this.distanceLength = distanceLength;
+        this.swimLength = swimLength;
+    }
+}
